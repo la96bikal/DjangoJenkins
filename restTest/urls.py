fea8084344a9f72ccd9238dd6841fb2927886018
Bikal Lamichhane <blamichhane@troy.edu>
2020-07-28 03:52:54 -0500

    another commit

--- restTest/urls.py
+++ restTest/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 #just a comment
-#just another comment
+#just another comment and another one
 urlpatterns = [
 	path('', views.hello, name = 'hello'),
 	path('<int:id>', views.hello)
