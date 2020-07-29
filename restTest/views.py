93c4e0dcae0da92f3bdba933ebd15f80d02063e4
Bikal Lamichhane <blamichhane@troy.edu>
2020-07-28 21:13:07 -0500

    Another commit for the demo

--- restTest/views.py
+++ restTest/views.py
@@ -6,5 +6,5 @@ def hello(request, id):
 	a = id
 	b = ''
 	for x in range(0, a+1):
-		b = b + str(x) +  "Hello world <br>"
+		b = b + str(x) +  "Hello world and the demo<br>"
 	return HttpResponse(b)
